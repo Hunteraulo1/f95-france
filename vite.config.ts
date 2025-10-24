58a377ea4df915fc08560fa6c42a1ee354c96518
Hunteraulo1 <hunteraulo@pm.me>
2025-10-24 15:59:00 +0200

    :bug: fixed =p

--- vite.config.ts
+++ vite.config.ts
@@ -2,27 +2,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import devtoolsJson from 'vite-plugin-devtools-json';
-import path from 'path';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit(), devtoolsJson()],
-	resolve: {
-		alias: {
-			'@lucide/svelte': path.resolve(__dirname, 'node_modules/@lucide/svelte/dist/lucide-svelte.js')
-		}
-	},
-	build: {
-		rollupOptions: {
-			external: []
-		}
-	},
-	optimizeDeps: {
-		include: ['@lucide/svelte']
-	},
-	ssr: {
-		noExternal: ['@lucide/svelte']
-	},
-	define: {
-		global: 'globalThis'
-	}
+	plugins: [tailwindcss(), sveltekit(), devtoolsJson()]
 });
