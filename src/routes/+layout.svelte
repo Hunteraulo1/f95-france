e2bd64643892f3338002b3fd7d50b50a64b54800
Hunteraulo1 <hunteraulo@pm.me>
2026-03-28 19:48:45 +0100

    :chart_with_upwards_trend: (frontend) implement analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,11 +3,16 @@
 	import { themeChange } from 'theme-change';
 	import '../app.css';
 
+  import { dev } from '$app/environment';
+  import { injectAnalytics } from '@vercel/analytics/sveltekit';
+
 	let { children } = $props();
 
 	onMount(() => {
 		themeChange(false);
 	});
+
+  injectAnalytics({ mode: dev ? 'development' : 'production' });
 </script>
 
 <svelte:head>
