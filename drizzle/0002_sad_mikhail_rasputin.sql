1397441e49dc259a404567bb4b4352c6a463dbb7
Hunteraulo1 <hunteraulo@pm.me>
2026-05-06 20:11:23 +0200

    :bug: (backend) make migrations 0001-0013 idempotent for existing prod

--- drizzle/0002_sad_mikhail_rasputin.sql
+++ drizzle/0002_sad_mikhail_rasputin.sql
@@ -1,2 +1,4 @@
-ALTER TABLE "game_translation" ADD COLUMN "game_type" varchar(32) DEFAULT 'other' NOT NULL;--> statement-breakpoint
-ALTER TABLE "game" DROP COLUMN "type";
+ALTER TABLE "game_translation"
+ADD COLUMN IF NOT EXISTS "game_type" varchar(32) DEFAULT 'other' NOT NULL;
+--> statement-breakpoint
+ALTER TABLE "game" DROP COLUMN IF EXISTS "type";
--- drizzle/0013_reflective_quicksilver.sql
+++ drizzle/0013_reflective_quicksilver.sql
@@ -1,2 +1,8 @@
-ALTER TABLE "user" ADD COLUMN "discord_id" varchar(255);--> statement-breakpoint
+ALTER TABLE "user"
+ADD COLUMN IF NOT EXISTS "discord_id" varchar(255);
+--> statement-breakpoint
+DO $$ BEGIN
 ALTER TABLE "user" ADD CONSTRAINT "user_discord_id_unique" UNIQUE("discord_id");
+EXCEPTION
+	WHEN duplicate_object THEN NULL;
+END $$;
