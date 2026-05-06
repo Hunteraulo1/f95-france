8ae2ebd8eb3f548df5a2c6a829fa734395eeeeae
Hunteraulo1 <hunteraulo@pm.me>
2026-05-06 10:01:34 +0200

    :whale: init docker

--- drizzle/0007_thin_sinister_six.sql
+++ drizzle/0007_thin_sinister_six.sql
@@ -1,1 +1,2 @@
-ALTER TABLE "api_key" ADD COLUMN "kind" varchar(16) DEFAULT 'bearer' NOT NULL;
+ALTER TABLE "api_key"
+ADD COLUMN IF NOT EXISTS "kind" varchar(16) DEFAULT 'bearer' NOT NULL;
--- drizzle/0012_bouncy_cammi.sql
+++ drizzle/0012_bouncy_cammi.sql
@@ -1,1 +1,2 @@
-ALTER TABLE "api_key" ADD COLUMN "total_request_count" integer DEFAULT 0 NOT NULL;
+ALTER TABLE "api_key"
+ADD COLUMN IF NOT EXISTS "total_request_count" integer DEFAULT 0 NOT NULL;
