a03955300ac9081cf73a97b253f12da208a6d5a0
Hunteraulo1 <hunteraulo@pm.me>
2026-05-15 10:54:14 +0200

    :chart_with_upwards_trend: imlplement insight

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,6 +13,7 @@
 <svelte:head>
 	<link rel="icon" href="/favicon.ico" />
 	<title>F95 France</title>
+  <script defer src="https://insight.f95france.site/script.js" data-website-id="7ba03446-3038-498c-be20-4c482a1cc3f1"></script>
 </svelte:head>
 
 {@render children()}
