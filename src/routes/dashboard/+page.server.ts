c2eea5fcbb6fc122989a1df23eab414cb30065df
Hunteraulo1 <hunteraulo@pm.me>
2026-05-25 12:46:02 +0200

    :bug: fix global stats

--- src/routes/dashboard/+page.server.ts
+++ src/routes/dashboard/+page.server.ts
@@ -37,6 +37,20 @@ export const load: PageServerLoad = async ({ locals }) => {
 				and(eq(table.submission.userId, locals.user.id), eq(table.submission.status, 'pending'))
 			);
 
+		const userOpenedSubmissionsResult = await db
+			.select({ count: sql<number>`count(*)`.as('count') })
+			.from(table.submission)
+			.where(
+				and(eq(table.submission.userId, locals.user.id), eq(table.submission.status, 'opened'))
+			);
+
+		const userToFixSubmissionsResult = await db
+			.select({ count: sql<number>`count(*)`.as('count') })
+			.from(table.submission)
+			.where(
+				and(eq(table.submission.userId, locals.user.id), eq(table.submission.status, 'to_fix'))
+			);
+
 		const userAcceptedSubmissionsResult = await db
 			.select({ count: sql<number>`count(*)`.as('count') })
 			.from(table.submission)
@@ -105,6 +119,8 @@ export const load: PageServerLoad = async ({ locals }) => {
 		userStats = {
 			totalSubmissions: toCount(userSubmissionsResult[0]?.count),
 			pendingSubmissions: toCount(userPendingSubmissionsResult[0]?.count),
+			openedSubmissions: toCount(userOpenedSubmissionsResult[0]?.count),
+			toFixSubmissions: toCount(userToFixSubmissionsResult[0]?.count),
 			acceptedSubmissions: toCount(userAcceptedSubmissionsResult[0]?.count),
 			gameAdd: toCount(userCounters?.gameAdd),
 			gameEdit: toCount(userCounters?.gameEdit),
@@ -115,6 +131,8 @@ export const load: PageServerLoad = async ({ locals }) => {
 		userStats = {
 			totalSubmissions: 0,
 			pendingSubmissions: 0,
+			openedSubmissions: 0,
+			toFixSubmissions: 0,
 			acceptedSubmissions: 0,
 			gameAdd: 0,
 			gameEdit: 0,
@@ -150,6 +168,16 @@ export const load: PageServerLoad = async ({ locals }) => {
 				.select({ count: sql<number>`count(*)`.as('count') })
 				.from(table.gameTranslation)
 				.where(eq(table.gameTranslation.status, 'abandoned'));
+
+			const submissionsOpenedResult = await db
+				.select({ count: sql<number>`count(*)`.as('count') })
+				.from(table.submission)
+				.where(eq(table.submission.status, 'opened'));
+
+			const submissionsToFixResult = await db
+				.select({ count: sql<number>`count(*)`.as('count') })
+				.from(table.submission)
+				.where(eq(table.submission.status, 'to_fix'));
 
 			const translationsUniqueTotalResult = await db
 				.select({ count: sql<number>`count(*)`.as('count') })
@@ -206,6 +234,8 @@ export const load: PageServerLoad = async ({ locals }) => {
 			const submissionsPending = toCount(submissionsPendingResult[0]?.count);
 			const submissionsAccepted = toCount(submissionsAcceptedResult[0]?.count);
 			const submissionsRejected = toCount(submissionsRejectedResult[0]?.count);
+			const submissionsOpened = toCount(submissionsOpenedResult[0]?.count);
+			const submissionsToFix = toCount(submissionsToFixResult[0]?.count);
 
 			stats = {
 				totalGames: toCount(totalGamesResult[0]?.count),
@@ -217,9 +247,16 @@ export const load: PageServerLoad = async ({ locals }) => {
 				},
 				submissions: {
 					pending: submissionsPending,
+					opened: submissionsOpened,
+					toFix: submissionsToFix,
 					accepted: submissionsAccepted,
 					rejected: submissionsRejected,
-					total: submissionsPending + submissionsAccepted + submissionsRejected
+					total:
+						submissionsPending +
+						submissionsAccepted +
+						submissionsRejected +
+						submissionsOpened +
+						submissionsToFix
 				},
 				totalUsers: toCount(totalUsersResult[0]?.count),
 				totalTranslators: toCount(totalTranslatorsResult[0]?.count),
@@ -241,6 +278,8 @@ export const load: PageServerLoad = async ({ locals }) => {
 				},
 				submissions: {
 					pending: 0,
+					opened: 0,
+					toFix: 0,
 					accepted: 0,
 					rejected: 0,
 					total: 0
