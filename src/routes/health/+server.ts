3b5111c8582299d7ae6538fa2e8117dd085bc5c5
Hunteraulo1 <hunteraulo@pm.me>
2026-06-12 14:01:19 +0200

    :rotating_light: lint code

--- src/routes/health/+server.ts
+++ src/routes/health/+server.ts
@@ -3,18 +3,18 @@ import { json } from '@sveltejs/kit';
 export const prerender = false;
 
 export const GET = () => {
-  return json(
-    {
-      status: 'ok',
-      service: 'f95-france',
-      uptime: process.uptime(),
-      timestamp: new Date().toISOString()
-    },
-    {
-      status: 200,
-      headers: {
-        'cache-control': 'no-store'
-      }
-    }
-  );
+	return json(
+		{
+			status: 'ok',
+			service: 'f95-france',
+			uptime: process.uptime(),
+			timestamp: new Date().toISOString()
+		},
+		{
+			status: 200,
+			headers: {
+				'cache-control': 'no-store'
+			}
+		}
+	);
 };
